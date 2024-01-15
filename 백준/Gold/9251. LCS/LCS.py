7be5fe82b142c432f811543ede97e3458ac95756
대선 <96492914+winterholic@users.noreply.github.com>
2024-01-16 00:35:13 +0900

    [Gold V] Title: LCS, Time: 552 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,18 @@
+A = input()
+B = input()
+
+#print(A, B)
+
+dp = [[0] * (len(B) + 1) for _ in range((len(A) + 1))]
+
+#print(dp)
+
+for i in range(1, len(A) + 1) :
+    for j in range(1, len(B) + 1) :
+        if(A[i - 1] == B[j - 1]) :
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else :
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+#print(dp)
+print(dp[len(A)][len(B)])
