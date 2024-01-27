b4735362e51acc12b6c98d5a41807831e2abf753
대선 <96492914+winterholic@users.noreply.github.com>
2024-01-27 13:18:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 35680 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,36 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+from collections import deque
+
+N, K = map(int, input().split())
+
+queue = deque()
+queue.append(N)
+
+visited = [-1 for _ in range(100001)]
+visited[N] = 0
+
+while queue :
+    x = queue.popleft()
+    #print(visited[:20])
+    if x == K :
+        break
+
+    x1 = x + 1
+    x2 = x - 1
+    x3 = 2 * x
+
+    if (x3 >= 0 and x3 < 100001) :
+        if(visited[x3] == -1) :
+            visited[x3] = visited[x] + 1
+            queue.appendleft(x3)
+    if (x2 >= 0 and x2 < 100001) :
+        if(visited[x2] == -1) :
+            visited[x2] = visited[x] + 1
+            queue.append(x2)
+    if (x1 >= 0 and x1 < 100001) :
+        if(visited[x1] == -1) :
+            visited[x1] = visited[x] + 1
+            queue.append(x1)
+
+print(visited[K])
