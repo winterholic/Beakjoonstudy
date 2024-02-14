584595da74210c386e3d94bc3ee8dd8523c6e642
대선 <96492914+winterholic@users.noreply.github.com>
2024-02-15 02:16:18 +0900

    [Silver III] Title: 01타일, Time: 396 ms, Memory: 69652 KB -BaekjoonHub

---
+++ 백준/Silver III/1904. 01타일/01타일.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp = [0] * (N + 1)
+
+for i in range(1, N + 1) :
+    if(i == 1) :
+        dp[i] = 1
+    elif(i == 2) :
+        dp[i] = 2
+    else :
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+print(dp[N])
