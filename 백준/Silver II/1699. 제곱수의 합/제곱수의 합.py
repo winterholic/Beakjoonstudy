71237094a39d591e103876bb8ebf353ee07d6bdb
대선 <96492914+winterholic@users.noreply.github.com>
2024-02-16 01:39:54 +0900

    [Silver II] Title: 제곱수의 합, Time: 6820 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver II/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver II/1699. 제곱수의 합/제곱수의 합.py
@@ -6,6 +6,7 @@ for i in range(1, N + 1) :
     for j in range(1, i) :
         if pow(j, 2) > i :
             break
-        dp[i] = min(dp[i], dp[i - pow(j, 2)] + 1)
-
+        if dp[i] > dp[i - pow(j, 2)] + 1 :
+            dp[i] = dp[i - pow(j, 2)] + 1
+            
 print(dp[N])
