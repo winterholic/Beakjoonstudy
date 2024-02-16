e4bb8c3f2d65c2c54c91ab6aad78fba9e29726e2
대선 <96492914+winterholic@users.noreply.github.com>
2024-02-16 18:45:25 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 532 ms, Memory: 252172 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -9,20 +9,22 @@ arr = [0]
 
 for number in numbers :
     if number > arr[len(arr) - 1] :
-        #print(number, arr[len(arr) - 1])
+        #print("case1", number, arr[len(arr) - 1], arr)
         arr.append(number)
     else :
         left = 0
-        right = len(arr)
+        right = len(arr) - 1
+        mini = 1000000000
 
-        while left < right :
+        while left <= right :
             mid = (right + left) // 2
-            #print(number ,mid, left, right)
-            if(arr[mid] < number) :
-                left = mid + 1
+            #print("case2", number ,mid, left, right, arr)
+            if(arr[mid] >= number) :
+                mini = min(mid, mini)
+                right = mid - 1
             else :
-                right = mid
-        arr[right] = number
+                left = mid + 1
+        arr[mini] = number
 
 arr.pop(0)
 #print(arr)
