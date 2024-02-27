205c1d18bcfe04c8e278ff3c1017030e10d57f38
대선 <96492914+winterholic@users.noreply.github.com>
2024-02-27 19:59:15 +0900

    [Gold V] Title: 숫자카드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2591. 숫자카드/숫자카드.py
@@ -0,0 +1,34 @@
+number = list(input())
+
+numbers = []
+
+for i in range(len(number)) :
+    if(number[i] == '0') :
+        temp = numbers.pop()
+        numbers.append(temp * 10)
+    else :
+        numbers.append(int(number[i]))
+#print(numbers)
+
+N = len(numbers)
+dp = [0] * N
+
+#print(numbers)
+
+#print(dp)
+
+dp[0] = 1
+
+for i in range(1, N) :
+    if (i == 1) :
+        if((numbers[i - 1] < 3 and numbers[i] < 10) or (numbers[i - 1] == 3 and numbers[i] < 5)) :
+            dp[1] = 2
+        else :
+            dp[1] = 1
+    else :
+        if((numbers[i - 1] < 3 and numbers[i] < 10) or (numbers[i - 1] == 3 and numbers[i] < 5)) :
+            dp[i] = dp[i - 2] + dp[i - 1]
+        else :
+            dp[i] = dp[i - 1]
+
+print(dp[-1])
