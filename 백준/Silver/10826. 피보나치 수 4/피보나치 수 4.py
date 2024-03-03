dbbad3c4fa97fdd47de281843f442fa785e68e0a
대선 <96492914+winterholic@users.noreply.github.com>
2024-03-04 03:58:38 +0900

    [Silver V] Title: 피보나치 수 4, Time: 76 ms, Memory: 34912 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+dp = [0] * (10001)
+
+for i in range(0, 10001) :
+    if(i == 0) :
+        dp[i] = 0
+    if(i == 1) :
+        dp[i] = 1
+    else :
+        dp[i] = dp[i - 2] + dp[i - 1]
+
+#print(dp)
+print(dp[N])
