8b160a22dd7bb43a47d173693aeb44284cefb678
대선 <96492914+winterholic@users.noreply.github.com>
2024-03-10 17:46:13 +0900

    [Gold V] Title: 용액, Time: 124 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+numbers = list(map(int, input().split()))
+
+anss = 0
+anse = len(numbers) - 1
+ansdata = abs(numbers[anss] + numbers[anse])
+
+s = 0
+e = len(numbers) - 1
+
+while(s < e) :
+    sumdata = numbers[s] + numbers[e]
+
+    if abs(sumdata) < ansdata :
+        ansdata = abs(sumdata)
+        anss = s
+        anse = e
+    
+    if sumdata < 0 :
+        s += 1
+    elif sumdata > 0:
+        e -= 1
+    else :
+        break
+
+print(numbers[anss], numbers[anse])
