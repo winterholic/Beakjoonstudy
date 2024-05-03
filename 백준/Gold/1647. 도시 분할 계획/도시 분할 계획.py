9c2e6d01baf78fd1f2e7c3a2eecce38810687c63
대선 <96492914+winterholic@users.noreply.github.com>
2024-05-03 17:06:13 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2676 ms, Memory: 208396 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+possible = []
+edgelist = []
+
+for _ in range(M) :
+    a, b, c = map(int, input().split())
+    possible.append((a - 1, b - 1, c))
+
+possible.sort(key = lambda x : x[2])
+
+parent_set = [i for i in range(N)]
+
+def find_parent(node):
+    if parent_set[node] == node:
+        return node
+    parent_set[node] = find_parent(parent_set[node])  # 경로 압축(Path Compression)을 통해 부모를 찾음
+    return parent_set[node]
+
+def Kruskal():
+    for k in possible:
+        node1 = k[0]
+        node2 = k[1]
+        weigh = k[2]
+        parent1 = find_parent(node1)
+        parent2 = find_parent(node2)
+        if parent1 == parent2:  # 두 노드의 부모가 같으면 사이클이 발생하므로 무시
+            continue
+        edgelist.append((node1, node2, weigh))
+        parent_set[parent2] = parent1  # 한 부모 아래로 합치기
+
+Kruskal()
+
+sum = 0
+
+sumlist = []
+
+for i in range(0, len(edgelist)) :
+    sumlist.append(edgelist[i][2])
+
+for sl in sumlist :
+    sum += sl
+
+sum -= max(sumlist)
+
+print(sum)
